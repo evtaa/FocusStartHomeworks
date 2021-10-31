6de9f0d54d0d83b46fd2305b073af057939d2875
Evtodiy Alexander <alex.evtodiy@gmail.com>
2021-10-31 17:35:30 +0300

    Убраны неиспользуемые фреймворки (тесты и CoreData). В папку проекта добавлен недостающий файл DetailsCarModuleBuilder.swift (в предыдущей версии файл был привязан к проекту, но находился вне папки проекта, что не позволило ревьюеру запустить проект). Из .gitignore удалено исключение файлов проекта *.xcodeproj/*. Комиты теперь находятся на ветке homework-1, названной в соответствии с техническим заданием.

---
+++ homeTask1/homeTask1/Modules/DetailsCar/Builder/DetailsCarModuleBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsCarModuleBuilder.swift
+//  homeTask1
+//
+//  Created by Alexandr Evtodiy on 31.10.2021.
+//
+
+import UIKit
+
+final class DetailsCarModuleBuilder {
+    static func build() -> DetailsCarViewController {
+        let vc = DetailsCarViewController()
+        return vc
+    }
+}
